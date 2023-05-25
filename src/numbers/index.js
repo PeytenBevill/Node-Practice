a1834dca4e11492174f18677ac5da2bfc5691364
PeytenBevill <redsage00@gmail.com>
2023-05-25 12:44:56 -0500

    got all 9 tests to pass

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,26 @@
 const isEven = (num) => {
   // write code for numbers.isEven
+  return num % 2 === 0
 
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+ return arr.reduce((acc, curr) => {
+  return acc + curr
+ }, 0)
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 1; j < arr.length; j++){
+      if(arr[i] + arr[j] === sum){
+        return true
+      }
+    }
+  }
+  return false
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,25 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split("-")
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
+  const res = []
 
+  for(let i = 0; i < str.length; i+=2){
+    res.push(str.slice(i, i + 2))
+  }
+  return res
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  let string = ''
+  for (let i = str.length - 1; i >= 0; i--){
+    string += str[i]
+  }
+  return string
 }
 
 module.exports = {
